794abef78771aa028a7aa54a3cfa022c3c704daa
raphael384 <abazerty384@gmail.com>
2024-03-30 16:56:45 +0100

    change auth

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,31 +1,18 @@
-import React, { useState } from "react";
 import bcrypt from "bcryptjs";
 
 interface Props {
   onEnter: (num: number) => void;
 }
 
-const Login = ({ onEnter }: Props) => {
-  const [scale, setScale] = useState(1);
+const Login = ({ onEnter }: Props) => {  
 
-  const changeScale = () => {
-    setScale((scale) => scale * 0.7);
-  };
 
-  const handleKeyPress = async (
-    event: React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    if (event.key === "Enter") {
-      const hash =
-        "$2a$04$K36gGv0adxOCStpMAsO.heBDuhdY/nkaHiAHj3ci6D7YiS5VAZ5ve";
-      if (await bcrypt.compare(event.currentTarget.value, hash)) {
-        onEnter(36);
-      } else {
-        changeScale();
-        console.log(
-          "debug hash : " + (await bcrypt.hash(event.currentTarget.value, 3))
-        );
-      }
+  const connect = async (password: string) => {
+    const hash = "$2a$04$K36gGv0adxOCStpMAsO.heBDuhdY/nkaHiAHj3ci6D7YiS5VAZ5ve";
+    if (await bcrypt.compare(password, hash)) {
+      onEnter(36);
+    } else {
+      console.log("debug hash : " + (await bcrypt.hash(password, 3)));
     }
   };
 
@@ -37,8 +24,7 @@ const Login = ({ onEnter }: Props) => {
           className="pwd-input"
           type="password"
           name="password"
-          onKeyDown={handleKeyPress}
-          style={{ transform: `scale(${scale})` }}
+          onChange={(e) => connect(e.target.value)}
         />
         <span onClick={() => onEnter(0)} className="invited">
           Accès Invité
